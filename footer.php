36dfc1fdf7c4b4ef7ab84e703351c48d61c300bc
Ola Albertsson <webb@aloq.se>
2016-04-19 11:26:57 +0200

    Lagt in Google Analytics.

--- footer.php
+++ footer.php
@@ -47,14 +47,15 @@
 	<script src="<?php echo get_template_directory_uri();?>/js/main.js?v=<?php echo time();?>"></script>
 	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		<?php if (!stristr($_SERVER['SERVER_NAME'], 'aloq')) {?>
-		ga('create', '-', 'auto');
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-76599818-1', 'auto');
 		ga('send', 'pageview');
-		<?php } ?>
+
 	</script>
+
 		<?php wp_footer(); ?>
 	</body>
 </html>
